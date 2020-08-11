6a0790481fa12e16bc81f7dcee47c3956aeaa511
Greg Hesp <greg.hesp@gmail.com>
2020-08-11 13:14:26 +0100

    docs: added Storage

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -18,10 +18,10 @@ module.exports = {
     Analytics: ["analytics/overview", toReferenceAPI("firebase_analytics")],
     Authentication: [
       "auth/overview",
-      // "auth/usage",
-      // "auth/social",
-      // "auth/phone",
-      // "auth/error-handling",
+      "auth/usage",
+      "auth/social",
+      "auth/phone",
+      "auth/error-handling",
       toReferenceAPI("firebase_auth"),
     ],
     "Cloud Firestore": [
@@ -29,22 +29,33 @@ module.exports = {
       "firestore/usage",
       toReferenceAPI("cloud_firestore"),
     ],
-    "Cloud Functions": ["functions/overview", toReferenceAPI("cloud_functions")],
+    "Cloud Functions": [
+      "functions/overview",
+      toReferenceAPI("cloud_functions")],
     // "Cloud Messaging": ["messaging/usage", toReferenceAPI("firebase_messaging")],
-    "Cloud Storage": ["storage/overview", "storage/usage", toReferenceAPI("firebase_storage")],
-    Core: ["core/usage", toReferenceAPI("firebase_core")],
+    Core: [
+      "core/usage",
+      toReferenceAPI("firebase_core")],
     Crashlytics: [
       "crashlytics/overview",
       "crashlytics/usage",
       "crashlytics/reports",
       toReferenceAPI("firebase_crashlytics")],
-    "Realtime Database": ["database/overview", toReferenceAPI("firebase_database")],
+    "Firebase Storage": [
+      "storage/overview",
+      "storage/usage",
+      toReferenceAPI("firebase_storage")],
+    "Realtime Database": [
+      "database/overview",
+      toReferenceAPI("firebase_database")],
     // "Dynamic Links": ["dynamic-links/usage", toReferenceAPI("firebase_dynamic_links")],
     // "Instance ID": ["iid/usage", toReferenceAPI("firebase_in_app_messaging")],
     // "In-App Messaging": ["in-app-messaging/usage", toReferenceAPI("firebase_in_app_messaging")],
     // "ML Kit Natural Language": ["ml-language/usage"],
     // "ML Kit Vision": ["ml-vision/usage", toReferenceAPI("firebase_ml_vision")],
     // "Remote Config": ["remote-config/usage", toReferenceAPI("firebase_remote_config")],
-    "Performance Monitoring": ["performance/overview", toReferenceAPI("firebase_performance")],
+    "Performance Monitoring": [
+      "performance/overview",
+      toReferenceAPI("firebase_performance")],
   },
 };
