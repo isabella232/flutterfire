8c5de870de2d3500dd3ec7c1428a5a33b1574394
Greg Hesp <greg.hesp@gmail.com>
2020-08-25 13:52:49 +0100

    feat: Cloud Functions Example App

---
+++ packages/cloud_functions/cloud_functions/example/functions/index.js
@@ -0,0 +1,12 @@
+const functions = require('firebase-functions');
+
+exports.listFruit = functions.https.onCall((data, context) => {
+   return [
+       "Apple",
+       "Banana",
+       "Cherry",
+       "Date",
+       "Fig",
+       "Grapes"
+   ]
+});
